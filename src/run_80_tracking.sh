dc99ddddaa76caa090fb68f7ad3df2fa4f7cfc10
Jessica Lee <you@example.com>
2020-08-13 18:40:44 -0400

    add execution script

---
+++ src/run_80_tracking.sh
@@ -0,0 +1,2 @@
+python demo.py tracking --exp_id baseline_run_driving1002 --load_model ../models/coco_tracking.pth --demo /data2/jl5/driving1/driving1002.mp4 \
+  --debug 1 --save_video --resize_video --video_w 1280 --video_h 720
