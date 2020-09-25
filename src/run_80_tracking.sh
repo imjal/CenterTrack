958c31f9e32fd8b7f83eab84e7d2485731b83da4
Jessica Lee <you@example.com>
2020-09-25 01:11:24 -0400

    stitched method

--- src/run_80_tracking.sh
+++ src/run_80_tracking.sh
@@ -1,2 +1,2 @@
-python demo.py tracking --exp_id baseline_driving$1 --load_model ../models/coco_tracking.pth --demo /data2/jl5/driving1/driving$1.mp4 \
+python demo.py tracking --exp_id stitched_driving$1 --load_model ../models/coco_tracking.pth --demo /data2/jl5/driving1/driving$1.mp4 \
   --debug 1 --record_mAP # --debug 4 --save_video
